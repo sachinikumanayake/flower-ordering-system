84c4d76cb84ac754c05adddb607c0a9ac3a0c6e0
sachinikumanayake <sachikumanayake003@gmail.com>
2025-08-04 13:54:59 +0530

    Backend

--- Backend/routs/userRoute.js
+++ Backend/routs/userRoute.js
@@ -3,4 +3,12 @@ import { loginUser,regiUser } from "../controllers/userController.js"
 const userRouter = express.Router()
 userRouter.post("/register",regiUser)
 userRouter.post("/login",loginUser)
+
+
+userRouter.get("/test", (req, res) => {
+    res.send("User route works!");
+  });
+  
+
+
 export default userRouter;
