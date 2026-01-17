2d07e25237d7159f20b895dc835ca2bc13aaa05e
sachinikumanayake <sachikumanayake003@gmail.com>
2026-01-17 21:02:54 +0530

    FIXED ISUUES

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,16 +1,14 @@
-
 import React, { useContext, useEffect } from "react"; 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import axios from 'axios'; 
 
 import StoreContextProvider from "../../shared/context/StoreContext.jsx"; 
 import AdminAuthContextProvider, {
-  AdminAuthContext,
+  AdminAuthContext,
 } from "./context/AdminAuthContext"; 
 
 import AdminLayout from "./layout/AdminLayout";
-
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Add from "./pages/Add/Add";
 import List from "./pages/List/List";
@@ -18,44 +16,48 @@ import Orders from "./pages/Orders/Orders";
 import Login from "./pages/Login/login";
 
 const AppContent = () => {
-  const url = "http://localhost:4000"; 
-  const { adminToken } = useContext(AdminAuthContext); 
+  const url = "http://localhost:4000"; 
+  const { adminToken } = useContext(AdminAuthContext); 
 
-  useEffect(() => {
-    if (adminToken) {
-      axios.defaults.headers.common['Authorization'] = `Bearer ${adminToken}`;
-    } else {
-      delete axios.defaults.headers.common['Authorization'];
-    }
-  }, [adminToken]); 
+  useEffect(() => {
+    if (adminToken) {
+      axios.defaults.headers.common['Authorization'] = `Bearer ${adminToken}`;
+    } else {
+      delete axios.defaults.headers.common['Authorization'];
+    }
+  }, [adminToken]); 
 
-  return (
-    <>
-      <ToastContainer />
-      <Routes>
-        {!adminToken ? (
-          <Route path="*" element={<Login />} /> 
-        ) : (
-          <Route path="/" element={<AdminLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="add" element={<Add />} /> 
-            <Route path="list" element={<List url={url} adminToken={adminToken} />} />
-            <Route path="orders" element={<Orders url={url} />} />
-          </Route>
-        )}
-      </Routes>
-    </>
-  );
+  return (
+    <>
+      <ToastContainer />
+      <Routes>
+        {!adminToken ? (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </>
+        ) : (
+          <Route path="/" element={<AdminLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="add" element={<Add />} /> 
+            <Route path="list" element={<List url={url} adminToken={adminToken} />} />
+            <Route path="orders" element={<Orders url={url} />} />
+            <Route path="login" element={<Navigate to="/" />} />
+          </Route>
+        )}
+      </Routes>
+    </>
+  );
 };
 
 const App = () => {
-  return (
+  return (
     <StoreContextProvider> 
       <AdminAuthContextProvider>
         <AppContent />
       </AdminAuthContextProvider>
     </StoreContextProvider>
-  );
+  );
 };
 
 export default App;
